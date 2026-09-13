7ec163395d09114e807acb7619b7ee4861924b4d
francistyron <francistyronlubasi@gmail.com>
2026-09-13 21:49:55 +0200

    Day 41 - Created relational tables based on an ER Diagram, structured customers, orders, products, and order details, loaded datasets into MySQL tables using import methods & practiced organizing data for relational analysis

---
+++ Queries/Day 41.sql
@@ -0,0 +1,56 @@
+use 50_Days_SQL_Challenge;
+
+
+---1. Create all tables based on the given ER diagram
+
+CREATE Table raw_customers(
+    customers_id varchar(50),
+    customer_name varchar(100),
+    city varchar(50),
+    signup_date varchar(50)
+);
+
+
+CREATE Table raw_orders(
+    order_id varchar(50),
+    customer_id varchar(50),
+    order_date varchar(50)
+);
+
+
+CREATE Table raw_products(
+    product_name varchar(50),
+    category varchar(50),
+    price int (50)
+);
+
+
+
+CREATE Table raw_order_details(
+    order_detail_id varchar(50),
+    product_id varchar(50),
+    quantity int(50)
+);
+
+
+---2. Load datasets into MYSQL tables using import/load methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
